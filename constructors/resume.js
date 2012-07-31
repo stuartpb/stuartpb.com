eb657e5a76c16bf73a5d15e49bddb1b4ee85bfe5
Stuart P. Bentley <stuart@testtrack4.com>
2012-07-30 18:27:38 -0700

    Jade-based templating

--- constructors/resume.js
+++ constructors/resume.js
@@ -2,9 +2,7 @@
 var fs = require('fs')
 
 // modules
-var marked = require('marked')
-var jsdom = require('jsdom').jsdom
-var xdate = require('xdate')
+var jade = require('jade')
 var yaml = require('js-yaml')
 
 // helpers / shortened functions
@@ -19,87 +17,13 @@ var extension = '.html'
 function construct(env) {
 
   //The skeletal HTML for the page.
-  var skelhtml = rfs('skeletons/' + pagename + extension)
+  var skelhtml = rfs('templates/resume.jade')
 
   //The data for the page.
   var data = require('../data/resume.yaml')
 
-  var document = jsdom(skelhtml);
-
-  //Element ID selector. If I wanted to pretend I was using jQuery I could name this '$'
-  function dgebi(id) {return document.getElementById(id)}
-
-  //Helper functions to construct elements
-  function techex() {
-    var techsets = data.techs
-    var setlist = document.createElement('ul')
-    for (var i=0; i < techsets.length; i++){ for(var name in techsets[i]){
-      var set = techsets[i][name]
-      var setitem = document.createElement('li')
-      setitem.textContent += name +": "
-      if(typeof(set)=="string") { //couldn't come up with a better way to do Hardware
-        setitem.innerHTML += marked(set)
-      } else {
-        for(var j=0;j<set.length;j++){
-          var skill = document.createElement('span')
-          skill.className = 'skill'
-          skill.textContent = set[j]
-          setitem.appendChild(skill)
-          //yes, really, this is the comma separation. no, I'm not proud.
-          if(j!=set.length-1) setitem.appendChild(document.createTextNode(', '))
-        }
-      }
-      setlist.appendChild(setitem)
-    }}
-    return setlist
-  }
-
-  function workex() {
-    function timespan(times) {
-      return times.from + ' - ' + times.to
-    }
-    var jobs = data.work
-    var joblist = document.createElement('ul')
-    for (var i=0; i < jobs.length; i++){
-      var job = jobs[i]
-      var jobitem = document.createElement('li')
-      jobitem.appendChild(document.createTextNode(
-        job.position + ', ' + job.organization + ', ' + timespan(job.timespan)))
-
-      jobitem.innerHTML += marked(job.tasks)
-
-      joblist.appendChild(jobitem)
-    }
-    return joblist
-  }
-
-  function education() {
-    var edus = data.education
-    var edulist = document.createElement('ul')
-    for (var i=0; i < edus.length; i++){
-      var edu = edus[i]
-      var eduitem = document.createElement('li')
-      eduitem.textContent= edu.accolade + ', ' + edu.institution
-      edulist.appendChild(eduitem)
-    }
-    return edulist
-  }
-
-  // Insert meat into skeleton
-  dgebi('tech-experience').appendChild(techex())
-
-  dgebi('work-experience').appendChild(workex())
-
-  var mdPortfolio = data.portfolio
-  dgebi('portfolio').innerHTML += marked(mdPortfolio)
-
-  dgebi('education').appendChild(education())
-
-  dgebi('version').textContent = env.describedVersion
-
-  dgebi('build-date').textContent = xdate().toString('MMM d yyyy')
-
-  return "<!DOCTYPE html>\n" + document.innerHTML
+  var fn = jade.compile(skelhtml,{pretty: true})
+  return fn({data: data, env: env, require: require})
 }
 
 exports.construct = construct
