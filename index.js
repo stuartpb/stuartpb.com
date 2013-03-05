e47821ea8b188c153675341a0d07fdb179e41c2a
Stuart P. Bentley <stuart@testtrack4.com>
2013-03-05 18:11:44 -0500

    Move infographic to info

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ var app = express();
 app.use(express.static(__dirname+'/static'));
 
 app.get('/resume(.html)?',require('./routes/resume/index.js'));
-app.get('/infographic',require('./routes/infographic/index.js'));
+app.get('/info',require('./routes/info/index.js'));
 
 module.exports = app;
