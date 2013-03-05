40949857028d481bc1362093be5abf12ac20421a
Stuart P. Bentley <stuart@testtrack4.com>
2013-03-05 06:07:10 -0500

    Increase the level of hair detail by a factor of ten

--- routes/infographic/hair.js
+++ routes/infographic/hair.js
@@ -4,8 +4,8 @@ var lastHaircut = {
 };
 
 function spbHairInMillimeters() {
-  return lastHaircut.hairMillimeters + // 27/256 mm per 6 hours
-    27/256 * (Date.now() - lastHaircut.time) / 21600000;
+  return lastHaircut.hairMillimeters + // 27/2048 mm per 36 minutes
+    27/2048 * (Date.now() - lastHaircut.time) / 2160000;
 }
 
 function updateHair(testLength){
@@ -15,7 +15,7 @@ function updateHair(testLength){
   document.getElementById("hairellipse").rx.baseVal.value = 50 +
     hairLength / 5;
     
-  document.getElementById("hairlength").textContent = hairLength.toFixed(2)
+  document.getElementById("hairlength").textContent = hairLength.toFixed(3)
     + ' mm';
     
   document.getElementById("hair-label-group").transform.baseVal.getItem(0)
