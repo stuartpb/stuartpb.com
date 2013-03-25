2a904ce4a6abd8f07996edc251a627ae5ee7bb52
Stuart P. Bentley <stuart@testtrack4.com>
2013-03-25 18:18:12 -0400

    Add additional routes for the Markdown resume

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ app.use(express.static(__dirname+'/static'));
 
 var resume = require('./routes/resume/index.js');
 
-app.get('/resume',resume.html);
-app.get('/resume.html',resume.html);
-app.get('/resume.md',resume.md);
-app.get('/resume.yaml',resume.yaml);
+app.get('/resume',      resume.html);
+app.get('/resume.html', resume.html);
+app.get('/resume.md',       resume.md);
+app.get('/resume.markdown', resume.md);
+app.get('/resume.txt',      resume.md);
+app.get('/resume.yaml', resume.yaml);
 
 app.get('/info',require('./routes/info/index.js'));
 app.get('/profiles',require('./routes/profiles/index.js'));
