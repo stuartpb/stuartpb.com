070ea8be08c6bc7acb10a0c94a17ec099a9641b0
Stuart P. Bentley <stuart@testtrack4.com>
2013-03-25 14:06:37 -0400

    Resume 4.2.0: Address moved from template to data
    This wasn't a big deal, until I had Markdown resume generation. Now it is.

--- routes/resume/index.js
+++ routes/resume/index.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var xdate = require('xdate');
 
 var dataFilename = __dirname+'/resume.yaml';
-var dataSource = fs.readFileSync(dataFilename,{encoding: "utf8"});
+var dataSource = fs.readFileSync(dataFilename,"utf8");
 var locals = yaml.load(dataSource, {filename: dataFilename});
 
 //allow Jade template to load marked and xdate by exposing require
@@ -15,37 +15,38 @@ exports.html = function(req,res,cb) {
 };
 
 function indent(str) {
-  return '    '+str.replace(/\n/g,'\n    ');
+  return '  '+str.replace(/\n/g,'\n  ').trim();
 }
 
 exports.md = function(req,res,cb) {
   res.set('Content-Type', 'text/markdown; charset=UTF-8');
-  res.send(["Stuart P. Bentley",
-    "500 Wall St, Apt. 1209, Seattle, WA 98121",
-    "Email: stuart@testtrack4.com Phone: +1-610-761-0054",
-    "",
-    "Tech Experience:",
+  //just so I don't go out of my mind with "locals.contact."s
+  var name = locals.contact.name;
+  var addr = locals.contact.address;
+  res.send([
+    '# '+[name.first,name.mi,name.last].join(' ')+ ' #',
+    addr.street + (addr.ext ? ", " + addr.ext : ' ') + ', '+
+      addr.city + ', ' + addr.state.abbr + ' ' + addr.zip,
+    "Email: " + locals.contact.email + " Phone: " + locals.contact.phone,
+    "\n## Tech Experience ##\n",
     locals.techs.map(function(category){for(var name in category){
       return '- ' + name + ': ' + (Array.isArray(category[name]) ?
         category[name].join(', ') :
         "\n" + indent(category[name]));
     }}).join('\n'),
-    "",
-    "Work Experience:",
+    "\n## Work Experience ##\n",
     locals.work.map(function(job){
       return '- '+job.position + ', ' + job.organization + ', ' +
         job.timespan.from + ' - ' + job.timespan.to + '\n' +
         indent(job.tasks);
     }).join('\n'),
-    "",
-    "Portfolio",
-    locals.portfolio,
-    "",
-    "Education",
+    "\n## Portfolio ##\n",
+    locals.portfolio.trim(),
+    "\n## Education ##\n",
     locals.education.map(function(edu){
       return '- '+edu.accolade + ', ' + edu.institution;
     }).join('\n'),
-    "",
+    "\n## About Resume ##\n",
     "Version "+locals.version+", "+xdate().toString('MMM d yyyy')+".",
     "The latest version of this resume is available in HTML format at " +
       locals.online.html+" and in Markdown format at " +
